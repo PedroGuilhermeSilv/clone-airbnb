4774c8ce643656b6b4770e9a96b863ac2dcab88f
guilherme <vinespedro55@gmail.com>
2024-05-09 17:44:54 -0300

    feat: :sparkles: Add ESLint and Prettier configuration files and VSCode settings

---
+++ frontend/nextbnb/app/components/navbar/NavBar.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const NavBar = () => {
+  return (
+    <nav className="w-full fixed top-0 left-0 py-6 border-b bg-white z-10">
+      <div className="max-w-[1500px] mx-auto px-3">
+        <div className="flex justify-between items-center">
+          <Link href="">
+            <Image width={180} height={38} alt="Logo bnb" src="/logo.png" />
+          </Link>
+          <div className="flex space-x-6">Search Filter</div>
+          <div className="flex space-x-6">Add property</div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
